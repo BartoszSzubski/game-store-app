0ca1de38181a76c01c3fa1fb727ff1db107b2bdf
BartoszSzubski <szubscy@gmail.com>
2026-04-25 15:34:04 +0200

    Add games data as objects and render dynamically instead of static HTML

---
+++ main.js
@@ -0,0 +1,43 @@
+import { games } from "./games.js";
+
+const gamesContainer = document.getElementById("games-container");
+//render gier
+
+function renderGames(list) {
+  gamesContainer.innerHTML = "";
+
+  list.forEach((game) => {
+    gamesContainer.innerHTML += `
+     <div class="games-grid_box">
+     <div class="box-image-section">
+              <img class="template-img" src="
+              ${game.image}" alt="" />
+              <div class="image-section_discount">
+              -${game.discountPercent}%</div>
+            </div>
+            <div class="under-box-section">
+              <p class="under-box-section_title">
+                ${game.name}
+              </p>
+              <p class="under-box-section_price">
+              ${game.price}zł</p>
+            </div>
+          </div>`;
+  });
+}
+
+renderGames(games.slice(0, 6));
+
+//obsługa searchButton
+const searchBtn = document.querySelector(".search-btn");
+const searchBar = document.querySelector(".search-bar");
+
+searchBtn.addEventListener("click", () => {
+  searchBar.classList.toggle("active");
+});
+
+document.addEventListener("click", (e) => {
+  if (!searchBar.contains(e.target) && !searchBtn.contains(e.target)) {
+    searchBar.classList.remove("active");
+  }
+});
